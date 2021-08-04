05f2808947d966fafedbc8dcea5132a12d9ab991
backgtx <wesleyafc2010@gmail.com>
2021-08-03 22:02:24 -0300

    create account

--- src/server.js
+++ src/server.js
@@ -1,10 +1,26 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
 app.use(express.json())
 
-app.get('/',(req,res)=>{
-    return res.json({message:"im in home page"})
+//to save data information
+const costumers = []
+
+app.post('/account',(req,res)=>{
+    const { cpf, name } = req.body
+    const id = uuidv4()
+    console.log(id)
+
+    //this will be save data from body
+    costumers.push({
+        cpf,
+        name,
+        id,
+        statements:[]
+    })
+
+    return res.status(201).send()
 })
 
 app.listen(3333)
