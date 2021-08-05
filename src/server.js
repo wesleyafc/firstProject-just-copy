cf87b8779ccd6debdb78793b1a431a2a822e3edb
backgtx <wesleyafc2010@gmail.com>
2021-08-04 22:14:45 -0300

    veryfy account exists middleware

--- src/server.js
+++ src/server.js
@@ -5,41 +5,58 @@ const app = express()
 app.use(express.json())
 
 //to save data information
-const costumers = []
+const customers = []
 
-app.post('/account',(req,res)=>{
+
+/**
+ *important*
+ *all routes bolow will use this middleware
+ *maybe can use to show error?
+*/
+//app.use(verifyAccountExist)
+
+//verify Account Exist with cpf in header
+function verifyAccountExist(req, res, next) {
+    //get cpf from params
+    const { cpf } = req.headers
+    //find and return cpf from customers array
+    const customer = customers.find(customer => customer.cpf === cpf)
+
+    if (!customer) {
+        return res.status(400).json({ error: "customer not found" })
+    }
+    //
+    req.customer = customer
+    return next()
+
+}
+
+app.post('/account', (req, res) => {
     const { cpf, name } = req.body
 
-    //find in arrays of costumers have same cpf
-    const customerAlreadyExists = costumers.some(
+    //find in arrays of customers have same cpf
+    const customerAlreadyExists = customers.some(
         (customer) => customer.cpf === cpf)
-    
-        //if customer already exists, show message and error
-    if(customerAlreadyExists){
-        return res.status(400).json({error:"customer already exists!!!"})
+
+    //if customer already exists, show message and error
+    if (customerAlreadyExists) {
+        return res.status(400).json({ error: "customer already exists!!!" })
     }
 
     //this will be save data from body
-    costumers.push({
+    customers.push({
         cpf,
         name,
-        id:uuidv4(),
-        statements:[]
+        id: uuidv4(),
+        statement: []
     })
 
     return res.status(201).send()
 })
 
-app.get('/statement/', (req, res) => {
-    //get cpf from params
-    const { cpf } = req.headers
-    //find and return cpf from costumers array
-    const costumer = costumers.find(costumer => costumer.cpf === cpf)
-
-    if(!costumer){
-        return res.status(400).json({error:"Costumer not found"})
-    }
-    return res.json(costumer.statements)
+app.get('/statement', verifyAccountExist, (req, res) => {
+    const { customer } = req
+    return res.json(customer.statement)
 })
 
 app.listen(3333)
